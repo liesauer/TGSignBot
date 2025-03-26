cf0329f6a863220697e89ec5da3e3357ba3e6fd0
LiesAuer <liesauer@liesauer.net>
2025-03-26 11:33:29 +0800

    修复默认langCode、systemLangCode问题

--- src/index.ts
+++ src/index.ts
@@ -163,8 +163,8 @@ async function main() {
         deviceModel: deviceModel || undefined,
         systemVersion: systemVersion || undefined,
         appVersion: appVersion || undefined,
-        langCode: langCode || undefined,
-        systemLangCode: systemLangCode || undefined,
+        langCode: langCode || "en",
+        systemLangCode: systemLangCode || "en-US",
     });
 
     await client.start({
